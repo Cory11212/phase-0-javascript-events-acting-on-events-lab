74535dafe00d1b37eb276690d0dec479fb49885f
Cory <corymclean0@gmail.com>
2023-08-22 22:19:47 -0400

    moving

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+﻿
+const dodger = document.getElementById("dodger");
+undefined
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+undefined
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+undefined
+function moveDodgerRight() {
+  const rightNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(rightNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+undefined
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
